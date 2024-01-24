9922e2d5bacf0e539d0c380056aa7074760e6fff
jorgeguedess <jorge_guedes00@hotmail.com>
2024-01-24 19:43:31 -0300

    feat: add project [2_JavaScript_Developer/02_music_player]

---
+++ 2_JavaScript_Developer/02_music_player/js/songs.js
@@ -0,0 +1,20 @@
+const songs = [
+  {
+    id: 1,
+    title: "Lost in the City Lights",
+    author: "Cosmo Sheldrake",
+    image: "/assets/imgs/cover-1.png",
+    alt: "A woman's face with a purple background",
+    url: "/assets/tracks/lost-in-city-lights-145038.mp3",
+  },
+  {
+    id: 2,
+    title: "Forest Lullaby",
+    author: "Lesfm",
+    image: "/assets/imgs/cover-2.png",
+    alt: "An image of a guitar with flowers in the background",
+    url: "/assets/tracks/forest-lullaby-110624.mp3",
+  },
+];
+
+export default songs;
